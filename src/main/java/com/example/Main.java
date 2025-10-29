69304b6f6ce408a40d12a7c8bc297e861a6378e3
abed7helmi <abed.helmi@yahoo.fr>
2025-10-29 15:48:30 +0100

    Convert strings to upper/lowercase using streams

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,8 +7,21 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) {
         //Write a Java program to calculate the average of a list of integers using streams.
-        averageStream();
+        //averageStream();
 
+        //Convert strings to upper/lowercase using streams
+        convertStream();
+
+    }
+
+    private static void convertStream() {
+        List<String> list = new ArrayList<>(List.of("ad", "bA", "cs"));
+
+        List<String> lower = list.stream().map(String::toLowerCase).collect(Collectors.toList());
+        List<String> uper = list.stream().map(String::toUpperCase).collect(Collectors.toList());
+
+
+        System.out.println(lower + "////" + uper);
     }
 
 
